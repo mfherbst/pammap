5ca67cc4134d30d6de295cdc518943748194f9af
Michael F. Herbst <info@michael-herbst.com>
2018-04-19 20:45:48 +0200

    More tests

--- pammap/core/tests/PamMapTests.cpp
+++ pammap/core/tests/PamMapTests.cpp
@@ -66,6 +66,81 @@ TEST_CASE("PamMap tests", "[pammap]") {
     REQUIRE(m.at<const ArrayView<Float>>("farr") == farr);
   }
 
+  //
+  // ---------------------------------------------------------------
+  //
+
+  SECTION("Check for KeyError") {
+    PamMap m{};
+    m.update("i", i);
+
+    REQUIRE_THROWS_AS(m.at<Bool>("blubber"), KeyError);
+    REQUIRE_THROWS_AS(m.at<Integer>("blubb"), KeyError);
+    REQUIRE(m.at<Integer>("i") == i);
+  }
+
+  //
+  // ---------------------------------------------------------------
+  //
+
+  SECTION("Test update function") {
+    PamMap m;
+    Float d = 3.4;
+    m.update("double", d);
+    m.update("noref", 3.141592);
+    m.update("word", "some");
+    m.update("bool", true);
+    m.update("not bool", false);
+    m.update("farr", farr);
+    m.update("iarr", iarr);
+
+    REQUIRE(m.at<Float>("double") == d);
+    REQUIRE(m.at<Float>("noref") == 3.141592);
+    REQUIRE(m.at<Bool>("bool"));
+    REQUIRE_FALSE(m.at<Bool>("not bool"));
+    REQUIRE(m.at<String>("word") == "some");
+    REQUIRE(m.at<ArrayView<Float>>("farr") == farr);
+    REQUIRE(m.at<ArrayView<Integer>>("iarr") == iarr);
+  }
+
+  //
+  // ---------------------------------------------------------------
+  //
+
+  SECTION("Test retrieving data from const maps") {
+    PamMap m;
+    m.update("double", 1.24);
+    m.update("noref", 3.141592);
+    m.update("word", "some");
+    m.update("farr", farr);
+
+    const PamMap& mref{m};
+    REQUIRE(mref.at<Float>("double") == 1.24);
+    REQUIRE(mref.at<Float>("noref") == 3.141592);
+    REQUIRE(mref.at<String>("word") == "some");
+    REQUIRE(mref.at<ArrayView<Float>>("farr") == farr);
+  }
+
+  //
+  // ---------------------------------------------------------------
+  //
+
+  SECTION("Test at with default return") {
+    std::vector<Float> other{6., 5., 4., 2.};
+    ArrayView<Float> anderes(other);
+
+    PamMap m;
+    m.update("string", s);
+    m.update("integer", i);
+    m.update("farr", farr);
+
+    REQUIRE(m.at<Integer>("blubber", 4) == 4);
+    REQUIRE(m.at<String>("blub", "neun") == "neun");
+    REQUIRE(m.at("string", String("neun")) == s);
+    REQUIRE(m.at("123", farr) == farr);
+    REQUIRE(m.at("farr", anderes) == farr);
+  }
+
 }  // TEST_CASE
 }  // namespace tests
 }  // namespace pammap
